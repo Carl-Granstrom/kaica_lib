3eaf5e362931ac5ef6b225fc3dba75be7c505cae
Carl Granstrom <carl.grnstrm@gmail.com>
2019-04-15 12:38:21 +0200

    setting up for first test

--- src/main/java/kaica_lib/entities/Title.java
+++ src/main/java/kaica_lib/entities/Title.java
@@ -2,10 +2,9 @@ package kaica_lib.entities;
 
 import org.hibernate.annotations.Type;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 /**
@@ -20,4 +19,67 @@ public class Title {
     @Type(type="uuid-char")
     @Column(nullable=false, unique=true)
     protected UUID uuid = UUID.randomUUID();
+
+    @Basic
+    @Column(name = "title")
+    public String title;
+
+    @Basic
+    @Column(name = "status")
+    public String status;
+
+    //todo placeholder, many-many relationship
+    @Basic
+    @Column(name = "author")
+    public String author;
+
+    @Temporal(TemporalType.DATE)
+    private Date retDate;
+
+    //todo: book does not use @temporal, needed?
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+
+
+
+    // ********************** Accessor Methods ********************** //
+
+    // ********************** Model Methods ********************** //
+
+    //todo placeholder with strings, enum?
+    //todo possibly throw exceptions down here instead of if/else?
+    //todo think about where we want to handle return dates
+    public boolean loan() {
+        if (this.status.equals("available")) {
+            this.status = "loaned";
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
+
+    // ********************** Common Methods ********************** //
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) {
+            return true;
+        }
+        if(!(obj instanceof Title)) {
+            return false;
+        }
+        Title title = (Title) obj;
+        return uuid != null && uuid.equals(title.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 }
